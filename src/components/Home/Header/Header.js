132776a057fa620858d53bb33130b452c2bbdaef
taauntik <devauntik@gmail.com>
2021-12-26 21:52:59 +0600

    made the navbar of home

---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// internal imports
+import Navbar from './Navbar'
+
+function Header() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// internal imports
+import Header from '../../components/Home/Header/Header'
+
+function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Home
