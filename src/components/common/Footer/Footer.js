e426b01811b80146ed2162797816c20ea6040233
taauntik <devauntik@gmail.com>
2021-12-28 11:37:39 +0600

    done full home page

---
+++ src/components/common/Footer/Footer.js
@@ -0,0 +1,95 @@
+import {
+  faFacebookF,
+  faGooglePlusG,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+// internal imports
+import "../../../styles/common/Footer/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 footer-col">
+            <br />
+            <br />
+            <small className="underline d-block mb-2">
+              Emergency Dental Care
+            </small>
+            <small className="underline d-block mb-2">Check Up</small>
+            <small className="underline d-block mb-2">
+              Treatment of Personal Diseases
+            </small>
+            <small className="underline d-block mb-2">Tooth Extraction</small>
+            <small className="underline d-block mb-2">Check Up</small>
+          </div>
+          <div className="col-md-3 footer-col">
+            <h6 className="primary-text mb-4">Services</h6>
+            <small className="underline d-block mb-2">
+              Emergency Dental Care
+            </small>
+            <small className="underline d-block mb-2">Check Up</small>
+            <small className="underline d-block mb-2">
+              Treatment of Personal Diseases
+            </small>
+            <small className="underline d-block mb-2">Tooth Extraction</small>
+            <small className="underline d-block mb-2">Check Up</small>
+            <small className="underline d-block mb-2">Check Up</small>
+            <small className="underline d-block mb-2">Check Up</small>
+          </div>
+          <div className="col-md-3 footer-col">
+            <h6 className="primary-text mb-4">Oral Health</h6>
+            <small className="underline d-block mb-2">
+              Emergency Dental Care
+            </small>
+            <small className="underline d-block mb-2">Check Up</small>
+            <small className="underline d-block mb-2">
+              Treatment of Personal Diseases
+            </small>
+            <small className="underline d-block mb-2">Tooth Extraction</small>
+            <small className="underline d-block mb-2">Check Up</small>
+            <small className="underline d-block mb-2">Check Up</small>
+          </div>
+          <div className="col-md-3 footer-col">
+            <h6 className="primary-text mb-4">Our Address</h6>
+            <small className="text-decoration-none">
+              New York - 101010 Hudson Yards
+            </small>
+            <br />
+            <br />
+            <div className="social-icons">
+              <div className="social-icon-container-first">
+                <FontAwesomeIcon
+                  icon={faFacebookF}
+                  className="social-icon social-icon-first"
+                />
+              </div>
+              <div className="social-icon-container">
+                <FontAwesomeIcon icon={faGooglePlusG} className="social-icon" />
+              </div>
+              <div className="social-icon-container">
+                <FontAwesomeIcon icon={faTwitter} className="social-icon" />
+              </div>
+            </div>
+            <br />
+            <br />
+            <small className="text-decoration-none">Call Now</small>
+            <br />
+            <button className="primary-btn">
+              <small className="text-decoration-none">+2025550295</small>
+            </button>
+          </div>
+        </div>
+        <br />
+        <br />
+        <small className="text-decoration-none copyright">Copyright {new Date().getFullYear()} All Rights Reserved</small>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // internal imports
 import Header from "../../components/Home/Header/Header";
@@ -9,8 +9,20 @@ import Featured from "../../components/Home/Featured/Featured";
 import Blogs from "../../components/Home/Blogs/Blogs";
 import Doctors from "../../components/Home/Doctors/Doctors";
 import Contact from "../../components/Home/Contact/Contact";
+import Footer from "../../components/common/Footer/Footer";
+import "../../styles/Home/Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
+  const [showFloatBtn, setShowFloatBtn] = useState(false);
+  useEffect(() => {
+    window.onscroll = function (e) {
+      // print "false" if direction is down and "true" if up
+      setShowFloatBtn(!(this.oldScroll > this.scrollY));
+      this.oldScroll = this.scrollY;
+    };
+  }, []);
   return (
     <>
       <Header />
@@ -21,6 +33,14 @@ function Home() {
       <Blogs />
       <Doctors />
       <Contact />
+      <Footer />
+      {showFloatBtn ? (
+        <div className="float-btn" onClick={() => window.scroll(0, 0)}>
+          <FontAwesomeIcon className="float-icon" icon={faArrowUp} />
+        </div>
+      ) : (
+        <div></div>
+      )}
     </>
   );
 }
